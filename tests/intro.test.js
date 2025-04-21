a40b7d72dd12d57e3ed2eb805916bdf19e9fc949
Doug <wanderlustking11@gmail.com>
2025-04-21 07:54:24 -0400

    Second Lesson: FizzBuzz Create 4 tests for fizzBuzz() All tests pass

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,6 +1,6 @@
 
 import { describe, test, it, expect } from 'vitest';
-import { max } from '../src/intro';
+import { fizzBuzz, max } from '../src/intro';
 
 describe('max', () => {
     it('should return the first argument if it is greater', () => {
@@ -24,5 +24,24 @@ describe('max', () => {
 
     it('should return the first argument if arguments are equal', () => {
         expect(max(2, 2)).toBe(2)
+    })
+})
+
+
+describe('fizzBuzz', () => {
+    it("should return 'FizzBuzz' if n is divisible by both 3 & 5", () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+
+    it("should return 'Fizz' if only divisible by 3", () => {
+        expect(fizzBuzz(9)).toBe("Fizz");
+    })
+
+    it("should return 'Buzz' if only divisible by 5", () => {
+        expect(fizzBuzz(10)).toBe("Buzz");
+    })
+
+    it("should return a number as a string if not divisible by 3 or 5", () => {
+        expect(fizzBuzz(11)).toBe("11");
     })
 })
